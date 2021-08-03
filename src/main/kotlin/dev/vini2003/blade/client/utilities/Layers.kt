3156842ebad74795c3b518bcbdc6ef7e29d5c977
vini2003 <personal@vini2003.dev>
2021-08-03 16:09:35 -0300

    Add interface render layer.

--- src/main/kotlin/dev/vini2003/blade/client/utilities/Layers.kt
+++ src/main/kotlin/dev/vini2003/blade/client/utilities/Layers.kt
@@ -16,6 +16,20 @@ class Layers(
 	companion object {
 		private val Layers = mutableMapOf<Identifier, RenderLayer>()
 		
+		val Interface = of(
+			"interface",
+			VertexFormats.POSITION_COLOR_LIGHT,
+			VertexFormat.DrawMode.QUADS,
+			256,
+			false,
+			true,
+			MultiPhaseParameters.builder()
+				.shader(Shader(GameRenderer::getPositionColorTexLightmapShader))
+				.transparency(RenderPhase.TRANSLUCENT_TRANSPARENCY)
+				.overlay(RenderPhase.DISABLE_OVERLAY_COLOR)
+				.build(true)
+		) as RenderLayer
+		
 		@JvmStatic
 		fun get(texture: Identifier): RenderLayer {
 			if (!Layers.containsKey(texture)) {

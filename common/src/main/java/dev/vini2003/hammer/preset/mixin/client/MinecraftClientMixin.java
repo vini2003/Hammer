a9ea47f047c1b8d0a3ded93739159664331146f7
vini2003 <vini.sch@outlook.com>
2024-01-18 14:13:54 -0300

    feat: url-based icon image loading

--- common/src/main/java/dev/vini2003/hammer/preset/mixin/client/MinecraftClientMixin.java
+++ common/src/main/java/dev/vini2003/hammer/preset/mixin/client/MinecraftClientMixin.java
@@ -24,29 +24,90 @@
 
 package dev.vini2003.hammer.preset.mixin.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import dev.vini2003.hammer.preset.HP;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.RunArgs;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.util.MacWindowUtil;
+import net.minecraft.client.util.Window;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWImage;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
+	@Shadow
+	@Final
+	private Window window;
+	
 	@Inject(at = @At("RETURN"), method = "getWindowTitle", cancellable = true)
 	private void hammer$getWindowTitle(CallbackInfoReturnable<String> cir) {
 		if (!HP.CONFIG.windowName.isEmpty()) {
 			cir.setReturnValue(HP.CONFIG.windowName + " - " + cir.getReturnValue());
 		}
+		
+		if (!HP.CONFIG.windowIcon.isEmpty()) {
+		
+		}
+	}
+
+
+	@Inject(at = @At("RETURN"), method = "<init>")
+	private void hammer$init$setIcon(RunArgs args, CallbackInfo ci) {
+		if (!HP.CONFIG.windowIcon.isEmpty()) {
+			try {
+				hammer$setIconFromUrl(HP.CONFIG.windowIcon);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
-//	@ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Lnet/minecraft/resource/ResourcePack;Lnet/minecraft/client/util/Icons;)V"), method = "<init>")
-//	private void hammer$init$setIcon(Args args) {
-//		// TODO: Reimplement this.
-//
-//		// args.set(0, HC.class.getResourceAsStream(HP.CONFIG.iconLowResPath));
-//		// args.set(1, HC.class.getResourceAsStream(HP.CONFIG.iconHighResPath));
-//	}
+	private void hammer$setIconFromUrl(String imageUrl) throws IOException {
+		RenderSystem.assertInInitPhase();
+		
+		var url = new URL(imageUrl);
+		
+		try (var is = url.openStream()) {
+			
+			try (var nativeImage = NativeImage.read(is)) {
+				if (MinecraftClient.IS_SYSTEM_MAC) {
+					MacWindowUtil.setApplicationIconImage(() -> is);
+				} else {
+					try (var memoryStack = MemoryStack.stackPush()) {
+						var buffer = GLFWImage.malloc(1, memoryStack);
+						
+						var byteBuffer = MemoryUtil.memAlloc(nativeImage.getWidth() * nativeImage.getHeight() * 4);
+						byteBuffer.asIntBuffer().put(nativeImage.copyPixelsRgba());
+						
+						buffer.position(0);
+						buffer.width(nativeImage.getWidth());
+						buffer.height(nativeImage.getHeight());
+						buffer.pixels(byteBuffer);
+						
+						GLFW.glfwSetWindowIcon(this.window.getHandle(), buffer);
+						
+						MemoryUtil.memFree(byteBuffer);
+					}
+				}
+			}
+		}
+	}
+	
 }
